ad5000185619d4c5a684d50ac0592e0f14e3c187
KarolChlasta <karol.chlasta@acm.org>
2020-08-08 17:29:44 +0200

    Updates

---
+++ precache-manifest.6c6f8212802fe0d2785d237c39345b7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7ddb22ca4d87e8a07366b98ffd3a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a472cd732e77b597ce0b",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a472cd732e77b597ce0b",
+    "url": "/static/js/main.db8d7397.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "45d3a9c9735d2dde89eb375f297b26ff",
+    "url": "/static/media/resume.45d3a9c9.pdf"
+  }
+]);
