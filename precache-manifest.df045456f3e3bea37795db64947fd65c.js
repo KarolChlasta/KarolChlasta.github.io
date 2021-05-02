0c25127626c5b8ea06ae8a61592545ba61dec905
Karol Chlasta <karol@chlasta.pl>
2021-05-02 02:06:16 +0200

    Publish the React portfolio site
    The original single-page portfolio built with Create React App, served at karol.chlasta.pl with the CV available for download.

---
+++ precache-manifest.df045456f3e3bea37795db64947fd65c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7339ef9225a222af411c0481b0354623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "786c1a737025dd977ebd",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786c1a737025dd977ebd",
+    "url": "/static/js/main.2b982900.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "87cd2f698561404ddce29a7e10cad9b7",
+    "url": "/static/media/resume.87cd2f69.pdf"
+  }
+]);
